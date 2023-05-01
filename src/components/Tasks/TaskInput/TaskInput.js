19665fe94bd00a75013197ea8c47ab26db6b9c5e
Izzat <superizzat564@gmail.com>
2023-05-01 17:32:31 +0500

    Add new features

--- src/components/Tasks/TaskInput/TaskInput.js
+++ src/components/Tasks/TaskInput/TaskInput.js
@@ -1,35 +1,34 @@
 import React, { useState } from "react";
-import styled from "styled-components";
 import Button from "../../UI/Button/Button";
-import "./TaskInput.css";
+import styles from "./TaskInput.module.css";
 
 
-const DivControl = styled.div`
-  margin: 0.5rem 0;
+// const DivControl = styled.div`
+//   margin: 0.5rem 0;
 
-& label {
-  font-weight: bold;
-  display: block;
-  margin-bottom: 0.5rem;
-  color: ${props => props.valid ? 'black' : 'red'}
-}
+// & label {
+//   font-weight: bold;
+//   display: block;
+//   margin-bottom: 0.5rem;
+//   color: ${props => props.valid ? 'black' : 'red'}
+// }
 
-& input {
-  display: block;
-  width: 100%;
-  border: 1px solid ${props => props.valid ? '#ccc' : 'red'};
-  font: inherit;
-  line-height: 1.5rem;
-  padding: 0 0.25rem;
-  background: ${props => props.valid ? 'tranparent' :  "rgb(197, 151, 151)"}
-}
+// & input {
+//   display: block;
+//   width: 100%;
+//   border: 1px solid ${props => props.valid ? '#ccc' : 'red'};
+//   font: inherit;
+//   line-height: 1.5rem;
+//   padding: 0 0.25rem;
+//   background: ${props => props.valid ? 'tranparent' :  "rgb(197, 151, 151)"}
+// }
 
-& input:focus {
-  outline: none;
-  background: #c8e1e4;
-  border-color: #00358b;
-}
-`;
+// & input:focus {
+//   outline: none;
+//   background: #c8e1e4;
+//   border-color: #00358b;
+// }
+// `;
 
 
 const TaskInput = (props) => {
@@ -54,10 +53,10 @@ const TaskInput = (props) => {
 
   return (
     <form onSubmit={formSubmitHandler}>
-      <DivControl valid={inputError}>
+      <div className={`${styles["form-control"]} ${!inputError && styles.valid} `}>
         <label>Задачи</label>
         <input type="text" onChange={taskInputChangeHandler} value={inputText} />
-      </DivControl>
+      </div>
       <Button type="submit">Добавить Задачу</Button>
     </form>
   );
